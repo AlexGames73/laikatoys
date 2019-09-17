42601cb5a307e778bd01301df6edfa1ac10e1822
APGames <permenev.alex@ya.ru>
2019-09-17 16:48:22 +0400

    start project

---
+++ src/main/java/com/tripplea/laikatoys/LaikatoysApplication.java
@@ -0,0 +1,13 @@
+package com.tripplea.laikatoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaikatoysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaikatoysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripplea/laikatoys/LaikatoysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripplea.laikatoys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaikatoysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
